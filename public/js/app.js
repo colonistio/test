3a882248d43377fab6c9f9b0008a4138531a395e
Majd Yahia <41284819+Majd-Yahia@users.noreply.github.com>
2021-09-28 12:42:35 +0300

    Update app.js with drawText function
    drawText will draw the score on the canvas.

--- public/js/app.js
+++ public/js/app.js
@@ -1,68 +1,73 @@
 var app = {
 	//initial variables
-	canvas  : null,
-	context : null,
+	canvas: null,
+	context: null,
 
 	//resizing
-	width   : 800,
-	height  : 400,
+	width: 800,
+	height: 400,
 
 	//nodes
-	nodes   : [],
+	nodes: [],
 
 	//timing
-	timestamp  : 0,
-	now        : 0,
-	lastUpdate : 0,
+	timestamp: 0,
+	now: 0,
+	lastUpdate: 0,
 
-	init : function(){
-		this.canvas  = document.getElementById('canvas');
+	init: function () {
+		this.canvas = document.getElementById('canvas');
 		this.context = this.canvas.getContext('2d');
 
 		this.render();
 		this.onInit();
 	},
-	render : function(){
+	render: function () {
 		this.clear();
 		this.update();
 
 		window.requestAnimationFrame(this.render.bind(this));
 	},
-	clear  : function(){
+	clear: function () {
 		this.context.clearRect(0, 0, this.width, this.height);
 	},
-	update : function(){
-	    var dt = Date.now() - this.lastUpdate;
+	update: function () {
+		var dt = Date.now() - this.lastUpdate;
 
 		this.onUpdate(dt);
 
-		for(var index in this.nodes){
+		for (var index in this.nodes) {
 			var node = this.nodes[index];
-
-			this.context.fillStyle = node.color;
-			this.context.fillRect(node.x, node.y, node.width, node.height);
-		}
-
-		this.lastUpdate = Date.now();
-		this.timestamp+=dt;
-	},
-	getNode : function(id){
-		for(var index in this.nodes){
-			var node = this.nodes[index];
-
-			if(node.id == id){
-				return node;
+			if (node.id === 'text') {
+				this.drawText(node);
+			} else {
+				this.context.fillStyle = node.color;
+				this.context.fillRect(node.x, node.y, node.width, node.height);
 			}
 		}
 
-		return { x : null, y : null, width : null, height : null };
+		this.lastUpdate = Date.now();
+		this.timestamp += dt;
+	},
+	getNode: function (id) {
+		for (var index in this.nodes) {
+			var node = this.nodes[index];
+			if (node.id == id) {
+				return node;
+			}
+		}
+		return { x: null, y: null, width: null, height: null };
+	},
+	drawText: function (node) {
+		this.context.font = "30px Arial";
+		this.context.fillText(node.text, node.x, node.y);
 	},
 
 	//events
-	onInit   : function(){},
-	onUpdate : function(){}
+	onInit: function () { },
+	onUpdate: function () { }
 };
 
-window.onload = function(){
+window.onload = function () {
 	app.init();
 };
