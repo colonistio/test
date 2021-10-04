7aeee0734f90382563f14606b10a43e4e8922d93
Majd Yahia <41284819+Majd-Yahia@users.noreply.github.com>
2021-10-04 16:30:26 +0300

    Adding Ball and collision modularity
    Adding ball module that contains ball logic. Also, collision interaction for both players and ball.

---
+++ public/js/ball.js
@@ -0,0 +1,41 @@
+// check if ball hit canvas borders. (left/Right)
+function BallHitNet(ball) {
+    if (ball == undefined) {
+        console.log("BallHitNet: ball is undefined");
+        return;
+    }
+
+    let xBallBounds = ball.x + ball.width;
+    if (xBallBounds > app.width) {
+        return 1;                                   // returns 1 if player one scored.
+    } else if (xBallBounds < 0) {
+        return 2;                                   // returns 2 if player two scored.
+    }
+    return 0;                                       // return 0 if it didnt hit the net.
+}
+
+
+// check if ball hit canvas borders. (Up/Down)
+function BallHitCanvas(ball) {
+    let yBallBounds = ball.y + ball.height;
+    if (ball.y <= 0 || yBallBounds >= app.height) { return true }
+    return false;
+}
+
+// Helper functions.
+function IncreaseBallSpeedPerHit(ball) { ball.speed += 0.9; }
+
+function ChangeBallDirection(ball) { ball.directionX = -ball.directionX; }
+
+function BallBounce(ball, angle) { ball.directionY = Math.tan(-angle) * ball.directionX; }
+
+function BallAngle(ball) { return Math.atan(ball.directionY / ball.directionX); }
+
+function BallLogic(ball, angle) {
+    ball.x += ball.directionX * ball.speed; // Move ball horizontally based on direction and speed.
+    ball.y += ball.directionY * ball.speed; // Move ball vertically based on direction and speed.
+    if (BallHitCanvas(ball)) { BallBounce(ball, angle); }
+    return BallHitNet(ball);
+}
+
+export { BallLogic, BallAngle, BallBounce, ChangeBallDirection, IncreaseBallSpeedPerHit }
---
+++ public/js/collision.js
@@ -0,0 +1,31 @@
+import { BallBounce, ChangeBallDirection, IncreaseBallSpeedPerHit } from './ball.js'
+
+// Gets the collision between two objects.
+function BallHitRacket(ball, player) {
+    if (ball == undefined) {
+        console.log("BallToPlayerCollision: ball is undefined!");
+        return;
+    }
+
+    if (player == undefined) {
+        console.log("BallToPlayerCollision: player is undefined!");
+        return;
+    }
+
+    return (
+        ball.x < player.x + player.width &&
+        ball.x + ball.width > player.x &&
+        ball.y < player.y + player.height &&
+        ball.y + ball.height > player.y
+    );
+}
+
+function BallRacketBounce(ball, angle, playerOne, playerTwo) {
+    if (BallHitRacket(ball, playerOne) || BallHitRacket(ball, playerTwo)) {
+        ChangeBallDirection(ball);
+        IncreaseBallSpeedPerHit(ball);
+        BallBounce(ball, angle);
+    }
+}
+
+export { BallRacketBounce }
