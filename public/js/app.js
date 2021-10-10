ba20d303ea7d59c382aa5c8ce681474a93e7b9b7
Jerick Ancheta <jerickson.ancheta@gmail.com>
2021-10-10 21:49:52 +0800

    test done according to template

--- public/js/app.js
+++ public/js/app.js
@@ -15,6 +15,7 @@ var app = {
 	now        : 0,
 	lastUpdate : 0,
 
+
 	init : function(){
 		this.canvas  = document.getElementById('canvas');
 		this.context = this.canvas.getContext('2d');
@@ -23,24 +24,64 @@ var app = {
 		this.onInit();
 	},
 	render : function(){
+
 		this.clear();
 		this.update();
-
+		
 		window.requestAnimationFrame(this.render.bind(this));
+
 	},
 	clear  : function(){
 		this.context.clearRect(0, 0, this.width, this.height);
 	},
+
+	pause  : function(){
+		this.onPause();
+	},
+
+	
+	reset  : function(){
+		this.onReset();
+	},
+
+	start  : function(){
+		this.onStart();
+	},
+
 	update : function(){
 	    var dt = Date.now() - this.lastUpdate;
 
 		this.onUpdate(dt);
 
 		for(var index in this.nodes){
+			
 			var node = this.nodes[index];
 
-			this.context.fillStyle = node.color;
-			this.context.fillRect(node.x, node.y, node.width, node.height);
+			if(node.type == "square"){
+					this.context.fillStyle = node.color;
+					this.context.fillRect(node.x, node.y, node.width, node.height);
+				
+			}else if(node.type == "text"){
+					this.context.font = node.font;
+					this.context.fillStyle = node.color;
+					this.context.fillText(node.input, node.x, node.y);
+					this.context.textAlign = node.align;
+					
+			}else if(node.type == "round"){
+			
+					this.context.beginPath();
+					this.context.arc(node.x, node.y, node.radius, 0, 2 * Math.PI, false);
+					this.context.fillStyle = node.color;
+					this.context.fill();
+				
+			
+			}else if(node.type == "line"){
+				this.context.beginPath();
+				this.context.setLineDash([25, 25]);
+				this.context.moveTo(node.x, 50);
+				this.context.lineTo(node.x, node.y);
+				this.context.stroke();
+			}
 		}
 
 		this.lastUpdate = Date.now();
@@ -60,7 +101,10 @@ var app = {
 
 	//events
 	onInit   : function(){},
-	onUpdate : function(){}
+	onUpdate : function(){},
+	onPause : function(){},
+	onReset : function(){},
+	onStart : function(){}
 };
 
 window.onload = function(){
