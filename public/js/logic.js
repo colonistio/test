97c1d354c3d2ada5800bec5cc4bd936c30f381de
djgundrum <44683554+djgundrum@users.noreply.github.com>
2022-01-20 13:02:19 -0500

    Update logic.js
    create updater function that is called to animate the screen. Handles all of the for ball moving and for ball hitting walls / going out of bounds. Calls functions to check if ball hit paddles and to move the bot.

--- public/js/logic.js
+++ public/js/logic.js
@@ -1,4 +1,49 @@
 /**
+ * Function that is called to run the main logic and animations
+ *
+ * main purposes:
+ *  1. update the position of the ball
+ *  2. make the ball bounce or check if it has hit a paddle
+ *    2a. if the ball goes out of bounds, reset game and tally score
+ *  3. increase ball speed if user has hit it certain amount of times
+ *  4. move the bot
+ */
+app.onUpdate = function (time) {
+	// if the user has paused the game, return
+	if (paused) return;
+
+	// get the ball
+	const ball = this.getNode("ball");
+
+	// update the position of the ball
+	ball.x = ball.x + ball.xv;
+	ball.y = ball.y + ball.yv;
+
+	// if the ball goes out of bounds vertically make it bounce
+	if (ball.y < 0 || ball.y > this.height) ball.yv = -ball.yv;
+
+	// if the ball goes out of bounds horizontally, add tally and restart
+	if (ball.x < 0 || ball.x > this.width) {
+		if (ball.x < 0) bot_score++;
+		else user_score++;
+
+		reset_game();
+	}
+	if (hits % 6 === 5) {
+		// reset the hits to prevent hits increasing each iteration
+		hits = 0;
+
+		// if the ball speed incremented won't warp through paddles
+		if (ball.xv + 0.5 < paddle_width) ball.xv += 0.5;
+	}
+
+	// check to see if the ball has hit either paddle
+	hit_paddle(ball);
+
+	// update the bots position
+	move_bot(ball);
+};
+/**
  * Function that handles each time the game restarts.
  *
  * Cases for this:
